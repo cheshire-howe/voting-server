ec4d75410d8d2c64bb3350bb116dbaff2be56cff
Josh Zoffranieri <joshzoff@gmail.com>
2016-06-19 20:11:33 -0400

    Store added to project

--- src/core.js
+++ src/core.js
@@ -37,8 +37,8 @@ export function next(state) {
 
 export function vote(state, entry) {
   return state.updateIn(
-      ['vote', 'tally', entry],
-      0,
-      tally => tally + 1
-      );
+    ['tally', entry],
+    0,
+    tally => tally + 1
+  );
 }
